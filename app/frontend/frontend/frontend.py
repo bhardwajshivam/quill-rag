c967dc8bb98de8ed45c2cd4ca9d34a41a4f85498
bhardwajshivam <bhardwajshivam121@gmail.com>
2024-05-01 15:50:23 -0400

    added editor box

--- app/frontend/frontend/frontend.py
+++ app/frontend/frontend/frontend.py
@@ -11,19 +11,24 @@ filename = f"{config.app_name}/{config.app_name}.py"
 class State(rx.State):
     """The app state."""
 
+class EditorState(rx.State):
+    content: str = "<p>Editor content</p>"
+
+    def handle_change(self, content: str):
+        """Handle the editor value change."""
+        self.content = content
+
 
 def index() -> rx.Component:
     return rx.center(
         rx.theme_panel(),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
+            rx.heading("Welcome to Quill RAG!", size="9"),
+            rx.text("Your RAG based essay writing copilot "),
+            rx.editor(
+                set_contents=EditorState.content,
+                on_change=EditorState.handle_change,
             ),
-            rx.logo(),
             align="center",
             spacing="7",
             font_size="2em",
