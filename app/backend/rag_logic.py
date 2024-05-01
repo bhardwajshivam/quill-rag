3af35bd4e443593c87b0286c1ed8a0ff87a49029
bhardwajshivam <bhardwajshivam121@gmail.com>
2024-05-01 11:11:36 -0400

    added rag

--- app/backend/rag_logic.py
+++ app/backend/rag_logic.py
@@ -1,1 +1,41 @@
-""" Module for RAG logic"""
+""" Module for RAG logic """
+
+import ollama
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+
+class Rag():
+    """ class defining the rag(retrieval augmented generation) logic """
+
+    def format_docs(self, docs):
+        """ function for joining the retrieved documents"""
+        return "\n\n".join(doc.page_content for doc in docs)
+
+
+    def load_vector_database(self):
+        """ function for calling retriever """
+        embeddings = OllamaEmbeddings(model = "mistral")
+        vector_store = Chroma(persist_directory = "app/data/vector-store",
+                              embedding_function = embeddings)
+        retriever = vector_store.as_retriever()
+        return retriever
+
+
+    def ollama_llm(self, question: str, context: str):
+        """ function for generating response using ollama """
+        formatted_prompt = f"""Question: {question} \n\n Context: {context}"""
+        response = ollama.chat(model = "mistral", 
+                               messages = [{'role': 'user', 'content': formatted_prompt}])
+        return response['message']['content']
+
+
+    def rag_chat_gen(self, question: str):
+        """ function for chat generation using rag """
+        retriever = self.load_vector_database()
+        retrieved_docs = retriever.invoke(question)
+        formatted_context = self.format_docs(retrieved_docs)
+        return self.ollama_llm(question, formatted_context)
+
+if __name__ == "__main__":
+    chat = Rag()
+    print(chat.rag_chat_gen(question="What is argumentation in LLM?"))
--- app/data/load_pdf_data.py
+++ app/data/load_pdf_data.py
@@ -3,29 +3,32 @@
 from os import path
 from glob import glob
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 embeddings = OllamaEmbeddings(model="mistral")
 
 list_data = []
 
-
-def find_ext(dr, ext):
+def find_ext(dr: str, ext: str):
     """ function for extracting pdf file names from the data directory. """
     x = glob(path.join(dr,"*.{}".format(ext)))
     return x
 
+# creating a list consisting names of pdf files
 list_data = find_ext("app/data/pdf-data", "pdf")
 loader = PyPDFLoader(list_data[0])
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap = 200)
 docs = loader.load_and_split(text_splitter)
 splits = text_splitter.split_documents(docs)
 embeddings = OllamaEmbeddings(model="mistral")
+
+# initializing vector_store with the first pdf file
 vector_store = Chroma.from_documents(documents = splits, embedding = embeddings,
                                      persist_directory="app/data/vector-store")
 vector_store.persist()
 
+# looping through the other pdfs and adding embeddings to our vector store
 for i in range(1, len(list_data)):
     try:
         cur_pdf = list_data[i]
@@ -37,4 +40,4 @@ for i in range(1, len(list_data)):
     except ValueError:
         print("Look into the file")
 
-# end of file
+# End-of-file (EOF)
