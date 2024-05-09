9ec4c2af534f364f1eeb76bb6855c83f1bd65575
bhardwajshivam <bhardwajshivam121@gmail.com>
2024-05-09 10:45:03 -0400

    added tests for cases with no editor content

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,12 +11,32 @@ def test_rag_chat_gen_default() -> None:
         test_flag = True
     assert test_flag is True
 
+def test_rag_chat_gen_with_no_context() -> None:
+    """ Function for testing the rag_chat_gen()"""
+    test_fn = Rag()
+    test_res = test_fn.rag_chat_gen("what is the need of argumentation and generation in language models?", "")
+    test_flag = False
+    if test_res is not None:
+        test_flag = True
+    assert test_flag is True
+
 def test_ollama_llm() -> None:
     """ Function for testing ollama """
     test_fn = Rag()
     question = "What is LLM?"
     context = "LLM stands for Large Language Model"
-    test_res = test_fn.ollama_llm(question, context)
+    test_res = test_fn.ollama_llm(question, context, editor_content = "editor content")
+    test_flag = False
+    if test_res is not None:
+        test_flag = True
+    assert test_flag is True
+
+def test_ollama_llm_with_empty_editor_content() -> None:
+    """ Function for testing ollama """
+    test_fn = Rag()
+    question = "What is LLM?"
+    context = "LLM stands for Large Language Model"
+    test_res = test_fn.ollama_llm(question, context, editor_content = "")
     test_flag = False
     if test_res is not None:
         test_flag = True
